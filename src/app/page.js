08e452ea54bda7c5f3a83fe79af9c02a1c78b18e
Стас <marinewc@Noutbuk-Stas.local>
2025-10-15 23:45:01 +0200

    add video

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import LinkList from "@/components/link-list/link-list";
 import Frame from "@/components/frame/frame";
 import { useState } from "react";
 import BackgroundVideo from 'next-video/background-video';
-import video from 'https://c02929e3-a4b9-4b2b-8aad-0bb85fe18283.selstorage.ru/123.mp4';
+import video from 'https://c02929e3-a4b9-4b2b-8aad-0bb85fe18283.selstorage.ru/000_FIN_2.mp4';
 
 export default function Home() {
 
